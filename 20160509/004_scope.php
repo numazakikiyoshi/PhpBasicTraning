19068c7328cf9146a378ae33d91258aafe2fbeaf
numazaki <numaki0110@yahoo.co.jp>
2016-05-09 18:34:00 +0900

    20160509

---
+++ 20160509/004_scope.php
@@ -0,0 +1,9 @@
+<?php
+function local() {
+    $var_local = "Hello ";
+    //global $var_global;
+    echo $var_local . $var_global ."\n";
+}
+$var_global = "World";
+local();
+?>
---
+++ 20160509/017_dot2.php
@@ -0,0 +1,7 @@
+<?php
+// 変数に文字列を格納
+$str_name1 = "沼崎 ";
+$str_name2 = "聖 ";
+// 文字列を連結して出力
+echo $str_name1 . $str_name2 . "でござる。\n";
+?>
